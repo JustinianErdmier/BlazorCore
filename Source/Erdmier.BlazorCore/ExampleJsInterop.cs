bdfe6bb7d30f7bf9526631e191bb6e0d27a6e022
JustinErdmier <justin.erdmier@outlook.com>
2022-11-09 23:02:57 -0600

    :tada: begin new project
    First commit for project.

---
+++ Source/Erdmier.BlazorCore/ExampleJsInterop.cs
@@ -0,0 +1,36 @@
+using Microsoft.JSInterop;
+
+namespace Erdmier.BlazorCore;
+
+// This class provides an example of how JavaScript functionality can be wrapped
+// in a .NET class for easy consumption. The associated JavaScript module is
+// loaded on demand when first needed.
+//
+// This class can be registered as scoped DI service and then injected into Blazor
+// components for use.
+
+public class ExampleJsInterop : IAsyncDisposable
+{
+    private readonly Lazy<Task<IJSObjectReference>> moduleTask;
+
+    public ExampleJsInterop(IJSRuntime jsRuntime)
+    {
+        moduleTask = new Lazy<Task<IJSObjectReference>>(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Erdmier.BlazorCore/exampleJsInterop.js").AsTask());
+    }
+
+    public async ValueTask<string> Prompt(string message)
+    {
+        IJSObjectReference module = await moduleTask.Value;
+
+        return await module.InvokeAsync<string>("showPrompt", message);
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        if (moduleTask.IsValueCreated)
+        {
+            IJSObjectReference module = await moduleTask.Value;
+            await module.DisposeAsync();
+        }
+    }
+}
